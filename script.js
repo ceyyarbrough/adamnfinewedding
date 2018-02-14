2dacdeec5a428f59c0db71e0e69bd3b9d74c726e
GoldenFarce <30757064+GoldenFarce@users.noreply.github.com>
2018-02-13 20:58:36 -0700

    Adding embedded quiz

---
+++ script.js
@@ -0,0 +1,7 @@
+var customerName = prompt("Please enter your name", "");
+if (customerName!= null) {
+    document.getElementById("welcome").innerHTML = "<h1>Hello " + customerName + "!</h1>"
+    document.getElementById("welcome").innerHTML += "<h1>Christopher and Nicole are pleased to invite you to attend their damn fine wedding.</h1>"
+}
+
+ 
