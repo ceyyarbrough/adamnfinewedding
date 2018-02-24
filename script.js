4f6ad3f43e94570b859d03b4108b3b1c148603e2
ceyyarbrough <christopher.e.yarbrough@gmail.com>
2018-02-23 18:22:50 -0700

    Modified alert

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var customerName = prompt("Please enter your name", "");
+var customerName = prompt("Who killed Laura Palmer? ", "");
 if (customerName!= null) {
     document.getElementById("welcome").innerHTML = "<h1>Hello " + customerName + "!</h1>"
     document.getElementById("welcome").innerHTML += "<h1>Christopher and Nicole are pleased to invite you to attend their damn fine wedding.</h1>"
